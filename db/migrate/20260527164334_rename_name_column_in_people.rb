7a73a6d6ef996a3e433155cc762aeadf68b7fba7
sharmakrupal06 <145786975+sharmakrupal06@users.noreply.github.com>
2026-05-27 16:45:09 +0000

    migrations-one

---
+++ db/migrate/20260527164334_rename_name_column_in_people.rb
@@ -0,0 +1,4 @@
+class RenameNameColumnInPeople < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
---
+++ db/migrate/20260527164359_rename_people_id_in_gifts.rb
@@ -0,0 +1,4 @@
+class RenamePeopleIdInGifts < ActiveRecord::Migration[8.0]
+  def change
+  end
+end
